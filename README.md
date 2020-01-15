99af1ef11ea42d12e3127a56019902cf698bc3d9
dmytro.malkin <malkin.dmytro@gmail.com>
2020-01-15 12:38:56 +0200

    add POST

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-1) docker build -t stem .
-2) docker run -p 5000:5000 stem 
+`docker build -t stem . && docker run -p 5000:5000 stem`
--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hey, we have Flask in a Docker container!'
+@app.route('/switch', methods=['POST'])
+def switch():
+    port = request.json['port']
+    #TODO add port switch with accroname
+    return req_data
 
 
 if __name__ == '__main__':
