aa3fcf2c8e48b58ba38d28d0ee381aefa873478f
dmytro.malkin <malkin.dmytro@gmail.com>
2020-01-15 15:53:38 +0200

    brainstem logic added

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import brainstem
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -5,9 +6,14 @@ app = Flask(__name__)
 
 @app.route('/switch', methods=['POST'])
 def switch():
+    id = request.json['id']
     port = request.json['port']
-    #TODO add port switch with accroname
-    return port
+
+    usbc_switch = brainstem.stem.USBCSwitch()
+    result = usbc_switch.discoverAndConnect(brainstem.link.Spec.USB, id)
+    usbc_switch.mux.setChannel(port)
+
+    return f'{result == 0}'
 
 
 if __name__ == '__main__':
